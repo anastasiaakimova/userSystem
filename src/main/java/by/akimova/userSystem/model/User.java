e0cccc290f2d11fe1bfc942829409d12efd6facf
anastasiaakimova <anastasiyaaki171@gmail.com>
2021-08-18 12:19:58 +0300

    Edit type of id

--- src/main/java/by/akimova/userSystem/model/User.java
+++ src/main/java/by/akimova/userSystem/model/User.java
@@ -1,11 +1,11 @@
 package by.akimova.userSystem.model;
 
 import lombok.Data;
-import org.bson.types.ObjectId;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.UUID;
 
 /**
  * @author anastasiyaakimava
@@ -17,13 +17,13 @@ public class User {
 
     @Field(name = "id")
     @Indexed(unique = true)
-    private String id;
+    private UUID id;
     @Field(name = "name")
     private String name;
     @Field(name = "password")
     private String password;
-    @Field(name = "telephone")
-    private String telephone;
+    @Field(name = "phone")
+    private String phone;
     @Field(name = "isActive")
     private Boolean isActive;
 
