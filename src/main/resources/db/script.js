2166b5ce61c0c2d6c1f9329f5372bd681ea976b2
anastasiaakimova <anastasiyaaki171@gmail.com>
2021-08-18 12:19:07 +0300

    Rename file

--- src/main/resources/db/script.js
+++ src/main/resources/db/script.js
@@ -2,10 +2,10 @@ db.createCollection("users")
 
 db.users.insertOne(
     {
-        "id": "1",
+        "id": "0cf6e724-fff4-11eb-9a03-0242ac130003",
         "name": "Tom",
         "password": "1234",
-        "telephone": "1234",
+        "phone": "1234",
         "isActive": false
     }
     )
@@ -14,26 +14,26 @@ db.users.createIndex({id:1}, {unique: true})
 
 db.users.insertMany([
     {
-        "id": "2",
+        "id": "e5bc1800-fff3-11eb-9a03-0242ac130003",
         "name": "Kate",
         "password": "0987",
-        "telephone": "7896",
+        "phone": "7896",
         "isActive": true
     },
 
     {
-        "id": "3",
+        "id": "ffb301e2-fff3-11eb-9a03-0242ac130003",
         "name": "Alex",
         "password": "0987rtyu",
-        "telephone": "789624357",
+        "phone": "789624357",
         "isActive": false
     },
 
     {
-        "id": "4",
+        "id": "06256fc4-fff4-11eb-9a03-0242ac130003",
         "name": "Mary",
         "password": "qwer0912387rtyu",
-        "telephone": "700000",
+        "phone": "700000",
         "isActive": true
     }
 ])
